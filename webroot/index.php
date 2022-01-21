1fa229d1bd7eddf20832ad748c0ceb5f73f2b505
Bassel AL ASHOUSH <basselalashoush@gmail.com>
2022-01-21 15:15:21 +0100

    edite webroot/index.php create constants

--- webroot/index.php
+++ webroot/index.php
@@ -1,3 +1,7 @@
 <?php
-
+define('WEBROOT', dirname(__FILE__));
+define('ROOT', dirname(WEBROOT));
+define('DS', DIRECTORY_SEPARATOR);
+define('CORE', ROOT . DS . 'core');
+define('BASE_URL', dirname(dirname($_SERVER['SCRIPT_NAME'])));
 echo "this is the webroot folder";
